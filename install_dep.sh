606b67ebae79a37964848313e3c9288e8c301432
Tùng Trần <59041428+tungtv289@users.noreply.github.com>
2025-05-27 13:45:30 +0700

    Create install_dep.sh

---
+++ install_dep.sh
@@ -0,0 +1,3 @@
+curl -Lo lazygit.tar.gz https://github.com/jesseduffield/lazygit/releases/download/v0.51.0/lazygit_0.51.0_Linux_x86_64.tar.gz
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
