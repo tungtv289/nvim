0b71cc4ea384f59e370961d69ecc28a10941ee66
Tùng Trần <59041428+tungtv289@users.noreply.github.com>
2025-05-27 13:52:17 +0700

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -1,3 +1,4 @@
+# install lazygit
 curl -Lo lazygit.tar.gz https://github.com/jesseduffield/lazygit/releases/download/v0.51.0/lazygit_0.51.0_Linux_x86_64.tar.gz
 tar xf lazygit.tar.gz lazygit
 rm -f lazygit.tar.gz
