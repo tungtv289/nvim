c9993833e8f3fbd60601036426f7c5ce4836e6da
Tùng Trần <59041428+tungtv289@users.noreply.github.com>
2025-06-03 16:52:09 +0700

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cp", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+  print("Copied file path to clipboard: " .. vim.fn.expand("%:p"))
+end, { desc = "Copy file path to clipboard" })
